79fc273d9b2bdae358f7f9a57be5c77aa11ccd48
skondaitservices <info@skonda.in>
2017-05-29 17:41:28 +0530

    Update comment

--- app/src/main/java/in/skonda/rmsapp/Login.java
+++ app/src/main/java/in/skonda/rmsapp/Login.java
@@ -2,7 +2,7 @@ package in.skonda.rmsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-// login screen - sudarsan.konda
+// login screen - sudarsan.konda . .
 public class Login extends AppCompatActivity {
 
     @Override
